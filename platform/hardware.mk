753eaa74f3d916810ad6ec1f8ac6c329d90306bb
russel5 <rnuriyev82@gmail.com>
2020-05-02 23:13:28 +0500

    init

---
+++ platform/hardware.mk
@@ -0,0 +1,44 @@
+### GRAPHICS
+PRODUCT_PACKAGES += \
+    copybit.msm8998 \
+    gralloc.msm8998 \
+    hwcomposer.msm8998 \
+    memtrack.msm8998 \
+    libdisplayconfig \
+    liboverlay
+
+### AUDIO
+PRODUCT_PACKAGES += \
+    libvolumelistener \
+    bthost_if
+
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat \
+    liboffloadhal
+
+# DISPLAY
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
+
+# SENSORS
+PRODUCT_PACKAGES += \
+    libsensorndkbridge
+
+# THERMAL
+PRODUCT_PACKAGES += \
+    thermal.msm8998
+
+# VR
+PRODUCT_PACKAGES += \
+    vr.msm8998
+
+# USB TRUST HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
+# COVER
+PRODUCT_PACKAGES += \
+    FlipFlap
